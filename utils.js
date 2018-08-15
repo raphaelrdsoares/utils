65329b70ef97452d9f73c5e8d772f8b42ba412b7
raphaelrdsoares <38190697+raphaelrdsoares@users.noreply.github.com>
2018-08-15 12:30:51 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -7,6 +7,21 @@ Array.prototype.clone = function() {
   return JSON.parse(JSON.stringify(this));
 };
 
+
+/* ---------------------------------------------------
+    VALIDATIONS
+----------------------------------------------------- */
+/**
+ * Verifica se o email é válido 
+ * 
+ * @param {String} email 
+ * @returns {boolean}
+ */
+export function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 /* ---------------------------------------------------
     UTILITIES 
 ----------------------------------------------------- */
